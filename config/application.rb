96b7440a1f934ba705f5cff09a04f687afba78b0
kai <jok002787@gmail.com>
2019-10-24 00:00:01 +0800

    revise i18n zh-tw tasks_controller and view

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,9 @@ Bundler.require(*Rails.groups)
 module Test18
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    
+    # setup i18n default zh-TW
+    config.i18n.default_locale = "zh-TW"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
