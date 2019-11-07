80a40c9abc6a72b408c00f62ce5dabf3e9d51767
kai <jok002787@gmail.com>
2019-11-07 16:01:47 +0800

    fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,17 @@
 #   Task.create(task: "吃飯飯", deadline: "20191026-18:00", content: "晚上吃飯飯", priority: 'low')
 #   puts "end"
 # }
-puts "Generating 10 Task..."
-10.times do |i|
-  Task.create(task: ["吃飯", "睡覺", "打球", "健身", "游泳", "看電影"].sample,
-                   deadline: Time.now,
-                   state: ["pending", "processing", "solved"].sample,
-                   priority: ['low', 'middle', 'high'].sample)
-end
 puts "Done!"
 
 puts "建立人造人1號"
-User.create(username: "人造人17號", email: "robot17@gmail.com", password: 123456)
+user1 = User.create(username: "人造人17號", email: "robot17@gmail.com", password: '123456')
 puts "以建立"
+
+puts "Generating 10 Task..."
+10.times do |i|
+  user1.tasks.build(task: ["吃飯", "睡覺", "打球", "健身", "游泳", "看電影"].sample,
+                    deadline: Time.now,
+                    state: ["pending", "processing", "solved"].sample,
+                    priority: ['low', 'middle', 'high'].sample)
+end
+puts "Done!"
